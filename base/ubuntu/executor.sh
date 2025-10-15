93970c06c43de8a0fa11dd597526a462ff7a02f8
Tobias Persson <tobiaspn@axis.com>
2025-10-15 13:30:25 +0200

    Add an ubuntu noble base dockerfile

---
+++ base/ubuntu/executor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+eval "$(pyenv init -)"
+
+PIP_ARGS=${PIP_ARG:-""}
+
+pyenv shell $ETR_PYTHON_VERSION
+
+DEV=${DEV:-false}
+if [ $DEV = "true" ] ; then
+    ETR_BRANCH=${ETR_BRANCH:-main}
+    ETR_REPOSITORY=${ETR_REPOSITORY:-https://github.com/eiffel-community/etos-test-runner.git}
+    ETR_INSTALL="git+$ETR_REPOSITORY@$ETR_BRANCH"
+else
+    ETR_INSTALL="etos_test_runner==$ETR_VERSION"
+fi
+
+pip install $PIP_ARGS $ETR_INSTALL
+
+exec python -m etos_test_runner.etr "$@"
