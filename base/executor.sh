a38df81373bcb3518826959bea2c1725b63db4e4
Andrei Matveyeu <andreimu@axis.com>
2025-12-01 08:32:35 +0100

    add timestamp logging to executor.sh (#35)

--- base/executor.sh
+++ base/executor.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+
+# Log a message with a timestamp.
+#
+# Arguments:
+#   $@ - The message to log.
+log() {
+    echo "[$(date --iso-8601=seconds)] - $*"
+}
+
+log "Sourcing pyenv."
 eval "$(pyenv init -)"
 
 PIP_ARGS=${PIP_ARG:-""}
 
-pyenv shell $ETR_PYTHON_VERSION
+log "Setting pyenv shell to version '$ETR_PYTHON_VERSION'."
+pyenv shell "$ETR_PYTHON_VERSION"
 
 DEV=${DEV:-false}
-if [ $DEV = "true" ] ; then
+if [ "$DEV" = "true" ] ; then
     ETR_BRANCH=${ETR_BRANCH:-main}
     ETR_REPOSITORY=${ETR_REPOSITORY:-https://github.com/eiffel-community/etos-test-runner.git}
     ETR_INSTALL="git+$ETR_REPOSITORY@$ETR_BRANCH"
@@ -14,6 +25,10 @@ else
     ETR_INSTALL="etos_test_runner==$ETR_VERSION"
 fi
 
-pip install $PIP_ARGS $ETR_INSTALL
+log "Installing ETR."
+pip install $PIP_ARGS "$ETR_INSTALL"
+log "ETR installed."
 
+log "Executing ETR."
 exec python -m etos_test_runner.etr "$@"
+log "ETR finished."
